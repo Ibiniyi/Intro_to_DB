63b77f6afac1285ea8ccaceecf10e762f94e3bc0
Timothy <timothyniyi@gmail.com>
2025-10-19 17:57:46 -0700

    task3

---
+++ task_3.sql
@@ -0,0 +1,9 @@
+-- =========================================================
+-- FILE: task_3.sql
+-- DESCRIPTION: Lists all tables in a MySQL database
+-- AUTHOR: Ibiniyi Timothy
+-- DATE: 2025-10-19
+-- =========================================================
+
+SHOW TABLES;
+
