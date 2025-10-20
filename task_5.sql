978ef736c6aa844615648fb4e010f53fede9e1ff
Timothy <timothyniyi@gmail.com>
2025-10-19 18:04:43 -0700

    task5

---
+++ task_5.sql
@@ -0,0 +1,11 @@
+-- =========================================================
+-- FILE: task_5.sql
+-- DESCRIPTION: Inserts a single row into the 'customer' table
+-- DATABASE: Passed as argument when executing MySQL command
+-- AUTHOR: Ibiniyi Timothy
+-- DATE: 2025-10-19
+-- =========================================================
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
