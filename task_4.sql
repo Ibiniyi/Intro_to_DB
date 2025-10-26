7a300ecb4ff9f0eab84f0ba7675daab284098f71
Timothy <timothyniyi@gmail.com>
2025-10-25 18:03:09 -0700

    task4

--- task_4.sql
+++ task_4.sql
@@ -1,18 +1,14 @@
--- =========================================================
--- FILE: task_4.sql
--- DESCRIPTION: Prints the full description of the table 'books'
--- DATABASE: Passed as argument when executing MySQL command
--- AUTHOR: Ibiniyi Timothy
--- DATE: 2025-10-19
--- NOTE: DESCRIBE or EXPLAIN statements are NOT used
--- =========================================================
+-- task_4.sql
+-- Print the full description of the table 'books' from the database alx_book_store
+-- without using DESCRIBE or EXPLAIN
 
-SELECT COLUMN_NAME, 
-       COLUMN_TYPE, 
-       IS_NULLABLE, 
-       COLUMN_KEY, 
-       COLUMN_DEFAULT, 
-       EXTRA
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = DATABASE()
   AND TABLE_NAME = 'books';
